3137b119bbdedf90154f541d65601e1e1795e0c5
spider1203 <spider1203@spider1203tekiMacBook-Pro.local>
2014-11-02 13:00:53 +0800

    Initial Commit

---
+++ GetViewControllerSizeTests/GetViewControllerSizeTests.m
@@ -0,0 +1,40 @@
+//
+//  GetViewControllerSizeTests.m
+//  GetViewControllerSizeTests
+//
+//  Created by spider1203 on 2014/11/2.
+//  Copyright (c) 2014年 kylelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GetViewControllerSizeTests : XCTestCase
+
+@end
+
+@implementation GetViewControllerSizeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
